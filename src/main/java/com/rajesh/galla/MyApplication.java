10297a2b5121aa3d1a91f68453c1df0d70be3def
rajeshgalla <rajeshbabu.galla@gmail.com>
2015-07-21 23:57:20 +0900

    Added the resource package to sub class of resourceconfig

--- src/main/java/com/rajesh/galla/MyApplication.java
+++ src/main/java/com/rajesh/galla/MyApplication.java
@@ -1,20 +1,22 @@
 package com.rajesh.galla;
 
 import com.rajesh.galla.resource.UserResource;
+import com.rajesh.galla.resource.UsersResource;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
 
 /**
  * Created by rajeshgalla on 7/9/15.
+ * Telling the Jersey where to look for resource classes
  */
+
+
 public class MyApplication extends ResourceConfig{
 
     public MyApplication() {
 
         System.out.println("MyApplication");
-        register(RequestContextFilter.class);
-        register(JacksonFeature.class);
-        register(UserResource.class);
+        packages("com.rajesh.galla.resource");
     }
 }
